61c305e0a6460fe55a3ec1b52ae4b1a3d31039ef
surafel sanga <61094420+surasong72@users.noreply.github.com>
2024-07-12 21:52:24 +0300

    This is an updated version of the app
    run the interface.py to start the recognition process

--- interface.py
+++ interface.py
@@ -1,84 +1,171 @@
-import tkinter as tk
-#from tkinter import ttk
-from tkinter import * 
-from tkinter import Canvas
-from PIL import Image, ImageTk
-import cv2
-# this is the function called when the button is clicked
-def btnClickFunction():
-	root.destroy()
-	cap.release()
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import tkinter as tk
+... from tkinter import *
+... from PIL import Image, ImageTk
+... import cv2
+... from keras.models import model_from_json
+... import operator
+... import numpy as np
+... 
+... # Loading the model
+... json_file = open("model-bw.json", "r")
+... model_json = json_file.read()
+... json_file.close()
+... loaded_model = model_from_json(model_json)
+... loaded_model.load_weights("model-bw.h5")
+... print("Loaded model from disk")
+... 
+... categories = {0: 'Aa', 1: 'ae', 2: 'be', 3: 'che', 4: 'chhe', 5: 'de', 6: 'fe',
+...               7: 'guh', 8: 'ha', 9: 'hea', 10: 'hha', 11: 'huh', 12: 'je', 13: 'keh',
+...               14: 'le', 15: 'me', 16: 'neh', 17: 'gne', 18: 'pe', 19: 'ppe', 20: 'qeh',
+...               21: 're', 22: 'se', 23: 'seh', 24: 'she', 25: 'teh', 26: 'tse', 27: 'tse1',
+...               28: 'Tte', 29: 'weh', 30: 'ye', 31: 'ze', 32: 'zjeh'}
+... 
+... # Function to stop the webcam and reset the GUI
+... def stop():
+...     button_start['state'] = NORMAL
+...     button_stop['state'] = DISABLED
+...     button_learn['state'] = NORMAL
+... 
+...     cap.release()
+...     label1.pack_forget()
+...     label.pack(side=tk.TOP, pady=5)
+...     frame_alphabet.pack_forget()
+...     label_display.pack(side=tk.TOP)
+...     label_ha.pack_forget()
 
+# Function to handle button click events for learning mode
+def clicked(text):
+    label1.pack_forget()
+    label_ha.pack(side=tk.TOP, pady=5)
 
+# Function to start learning mode
+def learn():
+    button_start['state'] = NORMAL
+    button_stop['state'] = NORMAL
+    button_learn['state'] = DISABLED
 
-root = Tk()
+    label.pack_forget()
+    label_display.pack_forget()
 
-# This is the section of code which creates the main window
-root.geometry('720x600')
-root.configure(background='#C1CDCD')
-root.title('\u121D\u120D\u12AD\u1275 \u124B\u1295\u124B')
+    frame_alphabet.pack(side=tk.TOP, pady=5)
+    label1.pack(side=tk.TOP, pady=5)
 
-frame_parent=Frame(root, width=700,height=600)
-frame_parent.grid(column=0,row=0)
-
-frame_top=Frame(frame_parent, width=80,height=100)
-frame_top.grid(column=0, row=0)
-
-# This is the section of code which creates the a label
-Label(frame_top, bg='#B9F3FC',text='\u12E8\u12A0\u121B\u122D\u129B \u121D\u120D\u12AD\u1275 \u124B\u1295\u124B \u1218\u121B\u122A\u12EB', height=4,width=50,font=('courier', 19, 'bold')).grid(row=0, column=0)
-
-frame_middle=Frame(frame_parent, width=700,height=200,bg="yellow")
-frame_middle.grid(column=0,row=1,padx=30,pady=20)
-
-frame_pic=Frame(frame_middle,width=200,height=200)
-frame_pic.grid(column=0,row=0,padx=50,pady=50)
-
-label=Label(frame_pic).grid(column=0,row=0,padx=40,pady=40)
-
-image = Image.open("C:\\Users\\sures\\Downloads\\sign.jpg")
-        # Resize the image using resize() method
-resize_image = image.resize((300, 200))
-img = ImageTk.PhotoImage(resize_image)
-        # create label and add resize image
-label = Label(image=img)
-label.image = img
-
-cap=cv2.VideoCapture(0)
-
-frame_button=Frame(frame_middle,width=80,height=100,bg="purple")
-frame_button.grid(column=1,row=1,pady=30,padx=30)
-
+# Function to capture frames from the webcam and make predictions
 def capture():
-        # Get the latest frame and convert into Image
-        cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
-        #cv2image.resize(400,450)
-        cv2.flip(cv2image,1)
-        img = Image.fromarray(cv2image)
-        # Convert image to PhotoImage
-        imgtk = ImageTk.PhotoImage(img)
-        label.imgtk= imgtk
-        label.configure(image=imgtk)
-        # Repeat after an interval to capture continiously
-        label.after(20, capture)
-        
-# This is the section of code which creates a button
-button_start=Button(frame_button, text='\u1300\u121D\u122D', height=2,width=8,bg='light cyan', font=('courier', 19, 'bold'), command=capture)
-button_start.grid(column=1,row=0)
+    button_start['state'] = DISABLED
+    button_stop['state'] = NORMAL
+    button_learn['state'] = NORMAL
 
+    label1.pack_forget()
 
-# This is the section of code which creates a button
-button_stop=Button(frame_button, text='\u12A0\u1241\u121D ',height=2,width=8, bg='light cyan', font=('courier', 19,'bold'),command=btnClickFunction)
-button_stop.grid(column=1,row=1)
+    # Get the latest frame and convert into Image
+    cv2img = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
+    cv2image = cv2.flip(cv2img, 1)
+    im_arr = np.asarray(cv2image)
+    img_arr_bgr = cv2.cvtColor(im_arr, cv2.COLOR_RGB2BGR)
 
-frame_bottom=Frame(frame_parent,width=300,height=200)
-frame_bottom.grid(column=0,row=2)
+    # Drawing the ROI
+    image_one = cv2.rectangle(img_arr_bgr, (400, 40), (630, 350), color=(255, 0, 0), thickness=1)
+    img_rgb = np.asarray(image_one)
+    image_rgb = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB)
+    im = Image.fromarray(image_rgb)
+    imgtk = ImageTk.PhotoImage(im)
+    label.imgtk = imgtk
+    label.configure(image=imgtk)
+    label.after(1, capture)
 
+    # Extracting the ROI
+    roi = image_rgb[40:350, 400:630]
+    roi = cv2.resize(roi, (64, 64))
+    test_image = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)[1]
+    result = loaded_model.predict(test_image.reshape(1, 64, 64, 3))
+    prediction = {categories[i]: result[0][i] for i in range(len(categories))}
+    prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
+    label_display.config(text=prediction[0][0])
 
-button_display=Button(frame_bottom, text='test',state=DISABLED, height=2,width=20,bg='light cyan', font=('courier', 19, 'bold'))
-button_display.grid(column=0,row=0,columnspan=2)
-#output=Text(root, height=5, width=35, bg=("light cyan")).place(x=45, y=386)
-#photo = PhotoImage(file = 'gesture.jpg')
-#root.iconphoto(False, photo)
+# Initialize the GUI
+root = Tk()
+root.title("\u121D\u120D\u12AD\u1275 \u124B\u1295\u124B")
+root.configure(bg="light cyan")
+root.maxsize(800, 768)
+root.geometry("800x768+270+0")
 
-root.resizable(False,False)
+ico = Image.open('sign.png')
+photo = ImageTk.PhotoImage(ico)
+root.wm_iconphoto(False, photo)
+
+cap = cv2.VideoCapture(0)
+
+frame_parent = Frame(root, width=300, height=80)
+frame_parent.pack(side=tk.TOP)
+
+label_title = Label(frame_parent, bg='#B9F3FC',
+                    text='\u12E8\u12A0\u121B\u122D\u129B \u121D\u120D\u12AD\u1275 \u124B\u1295\u124B \u1218\u121B\u122A\u12EB',
+                    height=4, width=60, font=('courier', 19, 'bold'))
+label_title.pack()
+
+frame_middle = Frame(root, width=200, height=300, bg='light cyan')
+frame_middle.pack(side=tk.RIGHT)
+
+button_start = Button(frame_middle, text='\u1300\u121D\u122D',
+                      height=2, width=8, bg='#84D2C5',
+                      font=('courier', 19, 'bold'), command=capture)
+button_start.pack(side=tk.TOP, pady=30, padx=20)
+
+button_stop = Button(frame_middle, text='\u12A0\u1241\u121D',
+                     height=2, width=8, bg='#84D2C5',
+                     font=('courier', 19, 'bold'), command=stop)
+button_stop.pack(side=tk.TOP)
+
+button_learn = Button(frame_middle, text='\u1218\u121B\u122A\u12EB',
+                      height=2, width=8, bg='#84D2C5',
+                      font=('courier', 19, 'bold'), command=learn)
+button_learn.pack(side=tk.BOTTOM, pady=30, padx=20)
+
+image = Image.open("sign.jpg")
+resize_image = image.resize((628, 482))
+img = ImageTk.PhotoImage(resize_image)
+
+label = Label(root, image=img)
+label.pack(side=tk.TOP, pady=5)
+
+frame_bottom = Frame(root, width=100, height=100, bg='light cyan')
+frame_bottom.pack(side=tk.TOP)
+
+label_display = Label(frame_bottom, width=10, height=2,
+                      text="", font=('courier', 19, 'bold'), bg="white")
+label_display.pack(side=tk.TOP, ipady=20)
+
+image1 = Image.open("amharic-alphabet.jpg")
+resize_image1 = image1.resize((628, 482))
+img1 = ImageTk.PhotoImage(resize_image1)
+
+label1 = Label(root, image=img1)
+
+frame_alphabet = Frame(frame_bottom, bg='black')
+
+# Add alphabet buttons
+buttons = [
+    ('\u1200', 'ha'), ('\u1208', 'le'), ('\u1210', 'hha'), ('\u1218', 'me'),
+    ('\u1220', 'seh'), ('\u1228', 're'), ('\u1230', 'se'), ('\u1238', 'she'),
+    ('\u1240', 'qeh'), ('\u1260', 'be'), ('\u1270', 'te'), ('\u1278', 'che'),
+    ('\u1280', 'hea'), ('\u1290', 'neh'), ('\u1298', 'nge'), ('\u12a0', 'Aa'),
+    ('\u12a8', 'keh'), ('\u12b8', 'huh'), ('\u12c8', 'weh'), ('\u12d0', 'ae'),
+    ('\u12d8', 'ze'), ('\u12e8', 'je'), ('\u12f0', 'de'), ('\u12f8', 'ge'),
+    ('\u1308', 'teh'), ('\u1310', 'Tte'), ('\u1320', 'che'), ('\u1328', 'tse'),
+    ('\u1338', 'tse1'), ('\u1348', 'guh'), ('\u1358', 'pe'), ('\u1380', 'fe')
+]
+
+for button_text, label_text in buttons:
+    Button(frame_alphabet, text=button_text, font=('courier', 19, 'bold'), width=5, height=1,
+           bg='#84D2C5', command=lambda text=label_text: clicked(text)).pack(side=LEFT, padx=2)
+
+image_ha = Image.open("ha.png")
+resize_image_ha = image_ha.resize((628, 482))
+img_ha = ImageTk.PhotoImage(resize_image_ha)
+
+label_ha = Label(root, image=img_ha)
+
 root.mainloop()
--- train.py
+++ train.py
@@ -1,74 +1,46 @@
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense, Dropout
-from keras import optimizers
-
-# Step 1 - Building the CNN
-
-# Initializing the CNN
-classifier = Sequential()
-
-# First convolution layer and pooling
-classifier.add(Convolution2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-# Second convolution layer and pooling
-classifier.add(Convolution2D(32, (3, 3), activation='relu'))
-# input_shape is going to be the pooled feature maps from the previous convolution layer
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-#Adding 3rd Concolution Layer
-classifier.add(Convolution2D(64, 3,  3, activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size =(2,2)))
-
-# Flattening the layers
-classifier.add(Flatten())
-
-#Step 4 - Full Connection
-classifier.add(Dense(128, activation = 'relu'))
-classifier.add(Dropout(0.5))
-classifier.add(Dense(9, activation = 'softmax'))
-
-# Compiling the CNN
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # categorical_crossentropy for more than 2
-
-
-# Step 2 - Preparing the train/test data and training the model
-
-from keras.preprocessing.image import ImageDataGenerator
-
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-training_set = train_datagen.flow_from_directory('data/train',
-                                                 target_size=(64, 64),
-                                                 batch_size=10,
-                                                 color_mode='rgb',
-                                                 class_mode='categorical')
-
-test_set = test_datagen.flow_from_directory('data/test',
-                                            target_size=(64, 64),
-                                            batch_size=10,
-                                            color_mode='rgb',
-                                            class_mode='categorical') 
-classifier.fit(
-        training_set,
-        steps_per_epoch=150, # No of images in training set
-        epochs=10,
-        validation_data=test_set,
-        validation_steps=35)# No of images in test set
-
-
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import numpy as np
+... import tensorflow as tf
+... from tensorflow.keras import layers, models, preprocessing
+... image_data_generator = preprocessing.image.ImageDataGenerator(rescale=1./255)
+... 
+... train_data_generator = image_data_generator.flow_from_directory(
+...     '/content/drive/MyDrive/Colab Notebooks/dataset/train',
+...     target_size=(64, 64),  # Resize images to 64x64 pixels
+...     batch_size=32,
+...     class_mode='categorical'
+... )
+... 
+... validation_data_generator = image_data_generator.flow_from_directory(
+...     '/content/drive/MyDrive/Colab Notebooks/dataset/test',
+...     target_size=(64, 64),
+...     batch_size=32,
+...     class_mode='categorical'
+... )
+... model = models.Sequential([
+...     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)),
+...     layers.MaxPooling2D((2, 2)),
+...     layers.Conv2D(64, (3, 3), activation='relu'),
+...     layers.MaxPooling2D((2, 2)),
+...     layers.Conv2D(128, (3, 3), activation='relu'),
+...     layers.MaxPooling2D((2, 2)),
+...     layers.Flatten(),
+...     layers.Dense(128, activation='relu'),
+...     layers.Dense(33, activation='softmax')  
+... ])
+... 
+... model.compile(optimizer='adam',
+...               loss='categorical_crossentropy',
+...               metrics=['accuracy'])
+... model.fit(
+...     train_data_generator,
+...     steps_per_epoch=len(train_data_generator),
+...     epochs=10,
+    validation_data=validation_data_generator,
+    validation_steps=len(validation_data_generator)
+)
 # Saving the model
 model_json = classifier.to_json()
 with open("model-bw.json", "w") as json_file:
     json_file.write(model_json)
-classifier.save_weights('model-bw.h5')
-
